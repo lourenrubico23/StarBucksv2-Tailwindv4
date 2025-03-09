52e83cde3b6064a9211738f545f496ed35f6ca89
unknown <lourenisobel18@gmail.com>
2025-03-09 14:46:00 +0800

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,40 @@
+/* Burger Button */
+const burger = document.querySelector(".burger__menu");
+const headerA = document.querySelector(".header__action");
+burger.addEventListener("click", () =>{
+    burger.classList.toggle("open");
+    headerA.classList.toggle("open");
+})
+
+// LINKS
+const links = document.querySelectorAll(".links__items h4");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    link.querySelector("i").classList.toggle("open");
+    link.nextElementSibling.classList.toggle("open");
+  });
+});
+
+// MENU TAB
+const starNav = document.querySelectorAll(".coffee__nav__link");
+const starContent = document.querySelectorAll(".coffee__item");
+starNav.forEach((star) => {
+  star.addEventListener("click", () => {
+    removeActiveStar();
+    star.classList.add("active");
+    const activeContent = document.querySelector(`#${star.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
+});
+
+function removeActiveStar() {
+  starNav.forEach((star) => {
+    star.classList.remove("active");
+  });
+}
+function removeActiveContent() {
+    starContent.forEach((content) => {
+      content.classList.remove("active");
+    });
+  }
